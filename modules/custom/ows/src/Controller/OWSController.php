94f11dcc38da9b853430b37b10c0eb6fac463297
Dan Tran <dantran@mandarindigital.com>
2016-03-09 22:55:38 +0700

    init

---
+++ modules/custom/ows/src/Controller/OWSController.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ @file
+ Contains \Drupal\ows\Controller\OWSController.
+ */
+
+namespace Drupal\ows\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class OWSController extends ControllerBase
+{
+	public function homepage() {
+		/*<div id="splash-img" style="display:none;">
+			<img src="/themes/ows_theme/images/splash.jpg" />
+    	</div>
+
+    	<div id="welcome-dialog" class="dialog" title="Welcome to OWS">
+  			<p>Lotem istest.</p>
+		</div>
+		
+		*/
+		
+		$account = \Drupal::currentUser();
+
+		if (!$account->uid) {
+			
+		} else {
+
+		}
+
+		$html = '<div id="welcome-box" class="" title="Welcome to OWS">
+			<div class="welcome-wrapper">
+  				<div class="welcome-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+  				<div class="buttons">
+  					<a href="#enter-content" id="btn-enter-contest" class="button button-red enter-contest">Enter the Contest</a>
+  					<a href="#vote" id="btn-sign" class="button button-red vote-contest">Vote in the Contest</a>
+  					<a href="#browse" id="btn-browse" class="button button-red browse-website">Browse the Website</a>
+  				</div>
+  			</div>
+		</div>';
+
+		$html .= '<div class="dialog">Test</div>';
+
+		return array('#type' => 'markup', '#markup' => $html);
+	}
+
+    public function ajaxContent() {
+    	$type = $_REQUEST['type'];
+    	$html = 'Register form 11';
+
+    	$form['name'] = array(
+		    '#type' => 'textfield',
+		    '#title' => t('Name'),
+		);
+
+		$html .= \Drupal::formBuilder()->getForm($form);
+
+        return array('#type' => 'markup', '#markup' => $html);
+    }
+}
---
+++ themes/ows_theme/ows.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function() {
+	// nice scrollbar
+    jQuery("html").niceScroll();
+
+	// splash screen
+
+	/*
+	jQuery('#splash-img').splashScreen({
+		textLayers : [
+			'/themes/ows_theme/images/thinner.png',
+			'/themes/ows_theme/images/more_elegant.png',
+			'/themes/ows_theme/images/our_new.png'
+		]
+	});	*/
+	
+	if (jQuery('body').hasClass('path-frontpage')) {
+		swal({
+			title: 'Welcome to OWS',
+			text: jQuery('#welcome-box').html(),
+			html: true,
+			customClass: 'twitter',
+			showConfirmButton: false
+			//allowEscapeKey: false
+		});
+
+		jQuery('#block-ows-theme-content #welcome-box').remove();
+		jQuery('.sweet-alert').center();
+	}
+
+
+    // enter contest
+    jQuery('#btn-enter-contest').on('click', function() {
+    	jQuery.ajax({
+			url: "/ajax-content",
+			data: {type: "register"},
+			async: false, 
+			success: function(data) {
+				jQuery('.dialog').html(data);
+			}
+		});
+
+    	swal.close();
+    	jQuery('.dialog').dialogr({
+    		open: function( event, ui ) {
+    			console.log(ui);
+    		}
+    	});
+    });
+});
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, ((jQuery(window).height() - jQuery(this).outerHeight()) / 2) + 
+                                                jQuery(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, ((jQuery(window).width() - jQuery(this).outerWidth()) / 2) + 
+                                                jQuery(window).scrollLeft()) + "px");
+    return this;
+}
